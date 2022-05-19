21e13bc27772b1e4794d4defab1cc4195ccca8c7
Brayan Pablo Puerto Coronado <brayan.ppc98@gmail.com>
2022-05-19 13:54:33 -0500

    Add configuration of location and hash navigation

---
+++ src/js/navigatorPage.js
@@ -0,0 +1,32 @@
+window.addEventListener('DOMContentLoaded', navigatorPage, false);
+window.addEventListener('hashchange', navigatorPage, false);
+
+function navigatorPage(){
+    if(location.hash.startsWith('#trends')){
+        trendsPage();
+    }else if(location.hash.startsWith('#search=')){
+        searchPage();
+    }else if(location.hash.startsWith('#movie=')){
+        moviePage();
+    }else if(location.hash.startsWith('#category=')){
+        categoryPage();
+    }else{
+        homePage();
+    }
+}
+
+function trendsPage(){
+    console.log('Trends page');
+}
+function homePage(){
+    console.log('Home page');
+}
+function searchPage(){
+    console.log('Search page');
+}
+function moviePage(){
+    console.log('Movie page');
+}
+function categoryPage(){
+    console.log('Categories page');
+}
