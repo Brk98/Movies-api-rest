5374568b409dcc2ce68e5dfbb01723f6a3d69385
Brayan Pablo Puerto Coronado <brayan.ppc98@gmail.com>
2022-05-20 16:06:18 -0500

    All the files are modulated and navigation working

--- src/js/navigatorPage.js
+++ src/js/navigatorPage.js
@@ -1,4 +1,7 @@
-
+/*Master*/
+import{getTopMovies} from './getTopMovies.js';
+import { getCatergoriesNames } from './getCategories.js';
+import{getUpcomingMoviesObject} from './getUpcomingMovies.js';
 
 window.addEventListener('DOMContentLoaded', navigatorPage, false);
 window.addEventListener('hashchange', navigatorPage, false);
@@ -22,6 +25,18 @@ function trendsPage(){
 }
 function homePage(){
     console.log('Home page');
+        /*Clousere para el lazy loading de Upcoming Movies*/
+        let closureUpcoming = function(){};
+        new Promise(function(resolve){
+            resolve(getUpcomingMoviesObject.getUpcomingMovies());
+        }).then(function(results){
+            closureUpcoming = getUpcomingMoviesObject.closureUpcomingMovies();
+            closureUpcoming.printUpcomingMovies();
+        })
+        /*Onclick Upcoming Movies*/
+        getUpcomingMoviesObject.loadMoreUpcomingMovies.addEventListener('click',function(){closureUpcoming.printUpcomingMovies();});
+    getTopMovies();
+    getCatergoriesNames();
 }
 function searchPage(){
     console.log('Search page');
