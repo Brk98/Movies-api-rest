b7288e5d64bf354c803a278588b59b4aeb038302
Brayan Pablo Puerto Coronado <brayan.ppc98@gmail.com>
2022-05-30 23:59:03 -0500

    Keyboard on android testing V6

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
 import { pages } from "./js/navigatorPage.js";
 import { nodes } from "./js/nodes.js";
 
+
+// Global boolean variable that holds the current orientation
+var pageInPortraitMode;
+
+// Listen for window resizes to detect orientation changes
+window.addEventListener("resize", windowSizeChanged);
+
+// Set the global orientation variable as soon as the page loads
 addEventListener("load", function() {
-    var viewport = document.querySelector("meta[name=viewport]");
-    viewport.setAttribute("content", viewport.content + ", height=" + window.innerHeight);
+  pageInPortraitMode = window.innerHeight > window.innerWidth;
+  document.getElementById("viewport").setAttribute("content", "width=" + window.innerWidth + ", height=" + window.innerHeight + ", initial-scale=1.0, maximum-scale=1.0, user-scalable=0");
 })
+
+// Adjust viewport values only if orientation has changed (not on every window resize)
+function windowSizeChanged() {
+  if (((pageInPortraitMode === true) && (window.innerHeight < window.innerWidth)) || ((pageInPortraitMode === false) && (window.innerHeight > window.innerWidth))) {
+    pageInPortraitMode = window.innerHeight > window.innerWidth;
+    document.getElementById("viewport").setAttribute("content", "width=" + window.innerWidth + ", height=" + window.innerHeight + ", initial-scale=1.0, maximum-scale=1.0, user-scalable=0");
+  }
+}
 
 nodes.search.addEventListener('click', function(){
     //remove default
